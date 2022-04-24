60a153f363b3ff6084352b8086234c7547d45f5b
kyowhei <kyoheitsapieceofcake@outlook.jp>
2022-04-25 01:55:43 +0900

    投稿機能の実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
    public function index(Post $post)
    {
     return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+   }
+   
+  /**
+ * 特定IDのpostを表示する
+ *引数の$postはid=1のPostインスタンス
+ */
+   public function show(Post $post)
+   {
+    return view('posts/show')->with(['post' => $post]);
+   }
+   
+   public function create()
+   {
+    return view('posts/create');
+   }
+   
+   public function store(PostRequest $request, Post $post)
+   {
+    $input = $request['post'];
+    $post->fill($input)->save();
+    return redirect('/posts/' . $post->id);
    }
 }
